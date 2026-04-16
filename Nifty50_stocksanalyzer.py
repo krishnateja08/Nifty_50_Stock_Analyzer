d0a905f5a0a895219002d41b0044713eea025072
krishnateja08 <krishnateja08@gmail.com>
2026-04-16 15:29:39 -0500

    Update Nifty50_stocksanalyzer.py

--- Nifty50_stocksanalyzer.py
+++ Nifty50_stocksanalyzer.py
@@ -182,7 +182,7 @@ class Nifty100CompleteAnalyzer:
             'SBILIFE.NS':     'SBI Life',
             'BPCL.NS':        'BPCL',
             'HDFCLIFE.NS':    'HDFC Life',
-            'LTIM.NS':        'LTIMindtree',
+            'LTIM.NS':        ,
             'ADANIENT.NS':    'Adani Enterprises',
             'SIEMENS.NS':     'Siemens India',
             # == NIFTY NEXT 50 =====================================
